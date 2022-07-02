2759812cf999f68311878622ce88c2c6313cedd9
Onose12 <onose@gmail.com>
2022-07-01 19:53:38 -0700

    qwe

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main -> assign a random number to the variable n each time it is executed
  * and print the last digit of the  number stored in the variable n
- * Retur: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
